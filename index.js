c15e5104d6ceb1e97008e2efbef0781973b15437
Hamza Younis <115169198+hamzayounis106@users.noreply.github.com>
2023-06-06 23:31:44 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-
+var accordionHeaders = document.querySelectorAll('.accordion-header');
+	accordionHeaders.forEach(function (header) {
+		header.addEventListener('click', function () {
+			// Toggle the display of accordion content
+			var content = this.nextElementSibling;
+			if (content.style.display === 'block') {
+				content.style.display = 'none';
+			} else {
+				content.style.display = 'block';
+			}
+		});
+	});
